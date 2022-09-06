babbb65c54ff62099e1760bb8b1c66530b31f10c
Patrick B <24831708+pbierkortte@users.noreply.github.com>
2022-09-06 01:04:48 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,11 +1,11 @@
-# Smallest (excluding whitespace)
-for i in range(1, 101):
-    print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i)
+# Shortest (ignoring whitespace)
+for i in range(100):
+    print("Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or i + 1)
 
 # Without using the modulo operator
 v = 810092048
-for i in range(1,101):
+for i in range(100):
     j = v & 3
     v = v >> 2 | j << 28
-    w = i, "Fizz", "Buzz", "FizzBuzz"
+    w = (i + 1, "Fizz", "Buzz", "FizzBuzz")
     print(w[j])
