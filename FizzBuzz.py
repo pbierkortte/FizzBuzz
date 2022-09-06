ef897b3bb58ebc443c8b400d546672e3be7ea9c0
Patrick B <24831708+pbierkortte@users.noreply.github.com>
2022-09-06 02:37:01 -0400

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -9,3 +9,11 @@ for i in range(100):
     v = v >> 2 | j << 28
     w = (i + 1, "Fizz", "Buzz", "FizzBuzz")
     print(w[j])
+
+# Without using the modulo or bitwise operators
+fizz = 0,0,1
+buzz = 0,0,0,0,1
+for idx in range(100):
+    fizz, buzz = fizz[1:] + fizz[:1], buzz[1:] + buzz[:1]
+    print("Fizz" * fizz[-1] + "Buzz" * buzz[-1] or idx + 1)
+    
