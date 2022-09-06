a4d1a2f4cccf0a14b577dec5b9dc9a5605e52848
Patrick B <24831708+pbierkortte@users.noreply.github.com>
2022-09-05 20:56:42 -0400

    Update FizzBuzz.sql

--- FizzBuzz.sql
+++ FizzBuzz.sql
@@ -6,7 +6,7 @@ with NUMBERS(I) as
 (               select I, 'FizzBuzz'            from NUMBERS where I % 15 = 0
     union all   select I, 'Fizz'                from NUMBERS where I %  3 = 0
     union all   select I, 'Buzz'                from NUMBERS where I %  5 = 0
-    union all   select I, cast(I AS varchar(8)) from NUMBERS 
+    union all   select I, cast(I AS varchar(8)) from NUMBERS
 )
 select max(S)
 from CTE
